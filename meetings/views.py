5199aece2a759ea54792bac68a797fa16f46e278
merveealpay <merveealpay@gmail.com>
2020-04-07 23:34:24 +0300

    added form

--- meetings/views.py
+++ meetings/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.forms import modelform_factory
 # Create your views here.
 
 from .models import Meeting, Room
-
+from .forms import MeetingForm
 
 def detail(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
@@ -12,3 +12,15 @@ def detail(request, id):
 
 def rooms_list(request):
     return render(request, "meetings/rooms_list.html", {"rooms": Room.objects.all()})
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
